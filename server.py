1ae1d1fd5032b8a3145080bc91e2441231ff8a32
hudemadhav <madhavhude1@gmail.com>
2026-06-15 09:51:09 -0400

    corrected code

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ def emotion_detect():
     # Pass the text to the emotion detector function and store the response 
     response = emotion_detector(text_to_analyze)
 
-    values_str = ", ".join(f"'{k}': {v}" for k, v in emotions.items() if k != "dominant_emotion")
+    values_str = ", ".join(f"'{k}': {v}" for k, v in response.items() if k != "dominant_emotion")
     # Return a formatted string with given format 
     result = f"For the given statement, the system response is {values_str}. The dominant emotion is {response['dominant_emotion']}."
     return result
@@ -21,4 +21,4 @@ def render_index_page():
     return render_template('index.html')
 
 if __name__ == "__main__":
-     app.run(debug=True)
+     app.run(host="0.0.0.0", port=5000, debug=True)
